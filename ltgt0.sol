b39c1143bfd30443cc74decc087154ed812e69ca
Flame57 <49445731+Flame57@users.noreply.github.com>
2022-07-15 20:35:27 +0800

    Create ltgt0.sol

---
+++ ltgt0.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+contract gasgas {
+    uint public g0;
+    uint public g1;
+    uint public g2;
+    uint public gas01;
+    uint public gas12;
+
+    function lt_gt() public {
+
+        g0 = gasleft();
+
+        for (uint i; i < 1000; ) {
+            if (i > 0) {}
+            unchecked { ++i; }
+        }
+
+        g1 = gasleft();
+
+        gas01 = g0 - g1;
+
+        g1 = gasleft();
+        for (uint i; i < 1000; ) {
+            if (i != 0) {}
+            unchecked { ++i; }
+        }        
+
+        g2 = gasleft();
+
+        gas12 = g1 - g2;
+
+    }
+
+}
